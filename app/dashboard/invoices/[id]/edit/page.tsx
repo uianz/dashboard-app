5a1cd6bac282d639499e13c2f047480f0ab917e9
uianz <zhuzhizhan@accompany.tech>
2024-04-11 15:40:56 +0800

    Chapter 13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import {fetchCustomers, fetchInvoiceById} from "@/app/lib/data";
 import Form from '@/app/ui/invoices/edit-form';
+import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
@@ -9,6 +10,10 @@ export default async function Page({ params }: { params: { id: string } }) {
         fetchInvoiceById(id),
         fetchCustomers(),
     ]);
+
+    if (!invoice) {
+        notFound();
+    }
     return (
         <main>
             <Breadcrumbs
